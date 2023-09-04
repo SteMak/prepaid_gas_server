3b8b571a8cb0b483fce667af48b7016e516c421f
SteMak <chestedos@gmail.com>
2023-09-04 10:43:02 +0200

    fix async init

--- cmd/validator/validator.go
+++ cmd/validator/validator.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"sync"
 
 	"github.com/joho/godotenv"
 
@@ -16,37 +15,23 @@ var (
 )
 
 func main() {
-	var wg sync.WaitGroup
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
 
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
+	err = config.Init()
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
 
-		err = godotenv.Load()
-		if err != nil {
-			log.Fatalln(err.Error())
-		}
+	err = db.Init()
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
 
-		err = config.Init()
-		if err != nil {
-			log.Fatalln(err.Error())
-		}
-	}()
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-
-		err = db.Init()
-		if err != nil {
-			log.Fatalln(err.Error())
-		}
-
-		err = http.Init()
-		if err != nil {
-			log.Fatalln(err.Error())
-		}
-	}()
-
-	wg.Wait()
+	err = http.Init()
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
 }
